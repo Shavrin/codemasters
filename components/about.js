ca8bf6599878ac02892c515160ffa9f1b533f9cb
KubaPralat <jakub.pralat@student.put.poznan.pl>
2018-05-27 20:04:27 +0200

    edited about

--- components/about.js
+++ components/about.js
@@ -2,5 +2,9 @@ Vue.component('about',{
 	template: `<div class="container"><div class="jumbotron">
   <h1 class="display-4">About</h1>
   <p class="lead">Codemasters is a simple platform to evaluate your javascript skills.</p>
+  <h1 class="display-4">Contact us</h1>
+  <p class="lead">If you need help, please contact us.</p>
+  <p class="lead">Kacper Olek: kacper.olek@student.put.poznan.pl</p>
+  <p class="lead">Jakub Prałat: jakub.pralat@student.put.poznan.pl</p>
 </div></div>`
 });
