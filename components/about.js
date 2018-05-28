aa0571dec4379216fe833ce02b4720daf035a9af
KubaPralat <jakub.pralat@student.put.poznan.pl>
2018-05-28 23:43:37 +0200

    edited about once again

--- components/about.js
+++ components/about.js
@@ -1,7 +1,8 @@
 Vue.component('about',{
 	template: `<div class="container"><div class="jumbotron">
   <h1 class="display-4">About</h1>
-  <p class="lead">Codemasters is a simple platform to evaluate your javascript skills.</p>
+  <p class="lead">Codemasters is a simple platform to evaluate your javascript skills. You have to 
+  do all the tasks in such a way that all tests are completed successfully</p>
   <h1 class="display-4">Contact us</h1>
   <p class="lead">If you need help, please contact us.</p>
   <p class="lead">Kacper Olek: kacper.olek@student.put.poznan.pl</p>
